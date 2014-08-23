113a3370679baefadd969a1613c492d5b2d0a27b
Joshua Freudenhammer <jmf316@gmail.com>
2014-08-23 12:17:18 -0500

    removing attempt to keep camera in border, will go back to that later. sifting through AT music now.

--- UnityProject/Assets/Scripts/CameraControl.cs
+++ UnityProject/Assets/Scripts/CameraControl.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
@@ -10,8 +10,25 @@ public class CameraControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 relPlayerPosition = GameObject.Find ("Player").transform.position - transform.position;
+
+
+
+		Vector3 playerPosition =  GameObject.Find ("Player").transform.position;
+		Vector3 clampedPosition = playerPosition;
+		//clampedPosition.x = Mathf.Clamp(playerPosition.x,0+(Camera.current.rect.width / 2),80-(Camera.current.rect.width / 2));
+
+		Vector3 relPlayerPosition = /*clampedPosition -*/playerPosition - transform.position;
 		relPlayerPosition.z = 0;
+		float relX = relPlayerPosition.x;
+		float camHalfWidth = (this.camera.rect.width /2 );
+
+		//need to ensure that the position to lerp to is WITHIN the bounds
+		//if((camera.rect.xMin >= relPlayerPosition.x) & (camera.rect.xMax <= relPlayerPosition.x))
+
+		//if (((0 + camHalfWidth) <= relX) & (relX <= (80 - camHalfWidth)))
+		//{
 		transform.Translate(Vector3.Lerp(Vector3.zero,relPlayerPosition,Time.deltaTime * 4f));
+		
+		//}
 	}
 }
