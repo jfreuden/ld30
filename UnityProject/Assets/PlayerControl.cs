52665562e2f3756c0ac081d691f179c1fce98816
Joshua Freudenhammer <jmf316@gmail.com>
2014-08-23 01:29:36 -0500

    Player moves, also: lerping.

---
+++ UnityProject/Assets/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+	public int moveSpeed = 4;
+	bool mirror;
+	// Use this for initialization
+	void Start ()
+	{
+			mirror = false;
+	}
+
+	void Update ()
+	{
+		float horizont = Input.GetAxis ("Horizontal");
+		if (horizont < 0) {
+			mirror = true;
+		} else if (horizont > 0) {
+			mirror = false;
+		}
+		if (mirror) {
+			this.transform.localScale = new Vector3 (-1, 1, 1);
+		} else {
+			this.transform.localScale = new Vector3 (1, 1, 1);
+		}
+		this.transform.Translate (horizont * moveSpeed * Vector2.right * Time.deltaTime);
+	}
+}
---
+++ UnityProject/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 relPlayerPosition = GameObject.Find ("Player").transform.position - transform.position;
+		relPlayerPosition.z = 0;
+		transform.Translate(Vector3.Lerp(Vector3.zero,relPlayerPosition,Time.deltaTime * 2.5f));
+
+	}
+}
