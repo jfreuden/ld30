671bcf28fd42eeaea45be7207ba135e600753abe
Joshua Freudenhammer <jmf316@gmail.com>
2014-08-23 17:58:18 -0500

    CAMERA NOT WORKING!!!!!

--- UnityProject/Assets/Scripts/CameraControl.cs
+++ UnityProject/Assets/Scripts/CameraControl.cs
@@ -1,34 +1,33 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
+				float minCamX = camera.ViewportToWorldPoint (Vector3.zero).x;
+				float maxCamX = camera.ViewportToWorldPoint (Vector3.one).x;
+				float camWidthHalf = (maxCamX - minCamX)/2;
+				print (minCamX.ToString () + " : " + maxCamX.ToString ());
 
+				Vector3 playerPosition = GameObject.Find ("Player").transform.position;
+				Vector3 relPlayerPosition = /*clampedPosition -*/playerPosition - transform.position;
+				relPlayerPosition.z = 0;
+				relPlayerPosition.x = Mathf.Clamp (relPlayerPosition.x,0 + camWidthHalf, 80-camWidthHalf);
+				//need to ensure that the position to lerp to is WITHIN the bounds
+				//if((camera.rect.xMin >= relPlayerPosition.x) & (camera.rect.xMax <= relPlayerPosition.x))
 
-
-		Vector3 playerPosition =  GameObject.Find ("Player").transform.position;
-		Vector3 clampedPosition = playerPosition;
-		//clampedPosition.x = Mathf.Clamp(playerPosition.x,0+(Camera.current.rect.width / 2),80-(Camera.current.rect.width / 2));
-
-		Vector3 relPlayerPosition = /*clampedPosition -*/playerPosition - transform.position;
-		relPlayerPosition.z = 0;
-		float relX = relPlayerPosition.x;
-		float camHalfWidth = (this.camera.rect.width /2 );
-
-		//need to ensure that the position to lerp to is WITHIN the bounds
-		//if((camera.rect.xMin >= relPlayerPosition.x) & (camera.rect.xMax <= relPlayerPosition.x))
-
-		//if (((0 + camHalfWidth) <= relX) & (relX <= (80 - camHalfWidth)))
-		//{
-		transform.Translate(Vector3.Lerp(Vector3.zero,relPlayerPosition,Time.deltaTime * 4f));
-		
-		//}
-	}
+				//if (((0 + camHalfWidth) <= relX) & (relX <= (80 - camHalfWidth)))
+				//{
+				transform.Translate (Vector3.Lerp (Vector3.zero, relPlayerPosition, Time.deltaTime * 4f));
+				//}
+		}
 }
