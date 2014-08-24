7bf8cc02ab1d90c7467f4299111e6e0073c15bb1
Joshua Freudenhammer <jmf316@gmail.com>
2014-08-23 19:38:13 -0500

    CAMERA WORKS!!!! I got the camera to obey level bounds. More stuff incoming. Also fixed gun pointing. Mirroring no longer interferes with accurate aim. Also upgrades incoming here too. Looking to mirror the player for "strafing"

--- UnityProject/Assets/GunPoint.cs
+++ UnityProject/Assets/GunPoint.cs
@@ -7,7 +7,7 @@ public class GunPoint : MonoBehaviour
 		// Use this for initialization
 		void Start ()
 		{
-
+			
 		}
 
 		// Update is called once per frame
@@ -19,19 +19,20 @@ public class GunPoint : MonoBehaviour
 				mousePos.z = 0;
 				Vector3 relMouse = mousePos - transform.position;
 				Vector3 relMouseNorm = relMouse.normalized;
-
+				
+				if (mirror) {
+						relMouseNorm.x *= -1;
+				} 
+				
 				GameObject player = GameObject.Find ("Player");
 				Vector3 relPlayer = player.transform.position - transform.position;
 				Vector3 relPlayerNorm = relPlayer.normalized;
 				Quaternion rotateToMouse;
 				rotateToMouse = Quaternion.FromToRotation (Vector3.right, relMouseNorm);
-				if (mirror) {
 				
-						rotateToMouse.z *= -1;
-						
-				} 
+				
 				//transform.rotation = rotateToPlayer;
-				//transform.rotation = rotateToMouse;
-
+				transform.rotation = rotateToMouse;
+				//transform.Rotate (transform.forward, Mathf.PI);
 		}
 }
--- UnityProject/Assets/Scripts/CameraControl.cs
+++ UnityProject/Assets/Scripts/CameraControl.cs
@@ -16,12 +16,15 @@ public class CameraControl : MonoBehaviour
 				float minCamX = camera.ViewportToWorldPoint (Vector3.zero).x;
 				float maxCamX = camera.ViewportToWorldPoint (Vector3.one).x;
 				float camWidthHalf = (maxCamX - minCamX)/2;
+				float minBorder = 0 + camWidthHalf;
+				float maxBorder = 80 - camWidthHalf;
 				print (minCamX.ToString () + " : " + maxCamX.ToString ());
 
 				Vector3 playerPosition = GameObject.Find ("Player").transform.position;
-				Vector3 relPlayerPosition = /*clampedPosition -*/playerPosition - transform.position;
+				playerPosition.x = Mathf.Clamp (playerPosition.x, minBorder, maxBorder);
+				Vector3 relPlayerPosition = playerPosition - transform.position;
 				relPlayerPosition.z = 0;
-				relPlayerPosition.x = Mathf.Clamp (relPlayerPosition.x,0 + camWidthHalf, 80-camWidthHalf);
+				//relPlayerPosition.x = Mathf.Clamp (relPlayerPosition.x,0 , 80-camWidthHalf);
 				//need to ensure that the position to lerp to is WITHIN the bounds
 				//if((camera.rect.xMin >= relPlayerPosition.x) & (camera.rect.xMax <= relPlayerPosition.x))
 
